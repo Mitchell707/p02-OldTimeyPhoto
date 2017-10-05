b5b281f0fa024a39971bcc02e0e1ba971544bde6
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2017-10-04 17:44:29 -0700

    prints the height and width of the photo.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,29 @@
 #include <string>
 #include "bitmap.h"
 
+using namespace std;
 
 int main()
 {
+    Bitmap image;
+    vector <vector <Pixel> > bmp;
+    Pixel rgb;
+
+    //read a file example.bmp and convert it to a pixel matrix
+    image.open("machupicchu.bmp");
+    bmp = image.toPixelMatrix();
+              
+
+    //take all the redness out of the top-left pixel
+    rgb = bmp[0][0];
+    rgb.red = 0; 
+
+    //put changed image back into matrix, update the bitmap and save it
+    bmp[0][0] = rgb;
+    image.fromPixelMatrix(bmp);
+    image.save("machupicchu.bmp");
+
+    cout << "machupicchu.bmp has been loaded. It is " << bmp[0].size() << " pixels wide and " << bmp.size() << " pixels high" << endl;
 
     return 0;
-
 }
