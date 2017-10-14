d8331637fef04c37ed6472f202c87ec1c93d401c
mbeck16@mail.csuchico.edu <mbeck16@ecc-jaguar.csuchico.edu>
2017-10-13 21:06:54 -0700

    Code will edit the image into gray scale or film grain.

--- main.cpp
+++ main.cpp
@@ -2,81 +2,85 @@
 #include <vector>
 #include <string>
 #include "bitmap.h"
+#include <cstdlib>
 
 using namespace std;
 
 string imgName;
+
+int temp = 0;
+
+int redEffect = 1;
+int greenEffect = 1;
+int blueEffect = 1;
+
+string editMode;
+
+bool chooseMode = true;
 
 int main()
 {
     Bitmap image;
     vector <vector <Pixel> > bmp;
     Pixel rgb;
-    
-    //read a file example.bmp and convert it to a pixel matrix
+
     do{
 
     cout << "Enter the name of the file you wish to edit.\n";
     cin >> imgName;
+        
+    image.open(imgName); //opens chosen bitmap image
     
-    //cout << imgName[imgName.length() - 4] << imgName[imgName.length() - 3] << imgName[imgName.length() - 2] << imgName[imgName.length() - 1] << endl;
-    
-    image.open(imgName);
-    
-    //cout << image.isImage();
-
     }while ( imgName[imgName.length() - 4] != '.' || imgName[imgName.length() - 3] != 'b' || imgName[imgName.length() - 2] != 'm' || imgName[imgName.length() - 1] != 'p' || image.isImage() == 0);
 
+    while (chooseMode == true)
+    {        
+        cout << "Enter the number of the effect you would you like to add to your image?" << endl << "1. Gray Scale\n" << "2. Film Grain\n";
+        cin >> editMode;
 
+        if(editMode == "1" || editMode == "2")
+        { 
+            chooseMode = false;
+        }
+    }
 
-    //image.open("machupicchu.bmp");
-    //image.open(imgName);
-    bmp = image.toPixelMatrix();
+    bmp = image.toPixelMatrix(); //
               
-    /*
-    //take all the redness out of the top-left pixel
-    rgb = bmp[0][0];
-    rgb.red = 0; 
-    */
-    
-    /*
-    //put changed image back into matrix, update the bitmap and save it
-    bmp[0][0] = rgb;
-    image.fromPixelMatrix(bmp);
-    image.save("machupicchu.bmp");
-    */
-
-    //cout << "machupicchu.bmp has been loaded. It is " << bmp[0].size() << " pixels wide and " << bmp.size() << " pixels high" << endl;
-    
+    //Make a multi if statement and have user input to edit the photo how they want, i.e. grayscale, film grain, negative, red, green, blue, purple, yellow, tiel, mirror.
     for(int r = 0; r < bmp.size(); r++)
     {
         for(int c = 0; c < bmp[r].size(); c++)
         {
-            rgb = bmp[r][c];
-            //convert to black and white
-    
-            int temp = (rgb.red + rgb.green + rgb.blue) / 3;
+            rgb = bmp[r][c]; //set rgb to the designated pixel in the array
             
-            rgb.red = temp;
-            rgb.green = temp;
-            rgb.blue = temp;
-            
-            //cout << temp << endl;
+            if(editMode == "1")
+            {
+                temp = (rgb.red + rgb.green + rgb.blue) / 3; //takes average of all colors in the pizel
+                rgb.red = temp; //sets all colors to the average
+                rgb.green = temp;
+                rgb.blue = temp;
+            }
+            else if(editMode == "2")
+            {
+                if(rand() % 12 == 0) //will make roughly 1 in every 12 pixels black
+                {
+                    temp = 0; 
 
-            bmp[r][c] = rgb;
+                    rgb.red = temp;
+                    rgb.green = temp;
+                    rgb.blue = temp;
+                }
+            }
+            bmp[r][c] = rgb; //sets the pixel to the resulting color
         }
     }
     
-    image.fromPixelMatrix(bmp);
+    image.fromPixelMatrix(bmp); //puts changed image back into matrix
     
     cout << "What name would you like to save this image with.\n";
     cin >> imgName;
 
-    image.save(imgName + ".bmp");
-    //image.save("machupicchuBlackAndWhite.bmp");
-    
-
-
+    image.save(imgName + ".bmp"); //saves the image with the name given by the user.
 
     return 0;
 }
